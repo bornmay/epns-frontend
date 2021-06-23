0ff9afc56f79870c3a7560e25e6d90514bb81ab6
Harsh Rajat <ProjectHydro@3602652.no-reply.drupal.org>
2021-06-24 03:37:12 +0530

    Updates

---
+++ precache-manifest.7c44ee467057ad0f73f79adf23b1e909.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f341f7529ab9ce1ebb57ccb45d0396bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38195f91cb0a5644de2d",
+    "url": "/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "4b7f120276b658512004",
+    "url": "/static/css/main.9ff9b3d6.chunk.css"
+  },
+  {
+    "revision": "38195f91cb0a5644de2d",
+    "url": "/static/js/2.5a69b39c.chunk.js"
+  },
+  {
+    "revision": "bc4afa37962f9b856391545ea94c3a64",
+    "url": "/static/js/2.5a69b39c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b7f120276b658512004",
+    "url": "/static/js/main.0725437b.chunk.js"
+  },
+  {
+    "revision": "3e234450efee118b7e12",
+    "url": "/static/js/runtime-main.fb220953.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0c7011cd02cfc0bf2372e8b8bb13e64c.js"
+  "/precache-manifest.7c44ee467057ad0f73f79adf23b1e909.js"
 );
 
 self.addEventListener('message', (event) => {
